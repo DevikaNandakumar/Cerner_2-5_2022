764808d92a8dcd096faf622eb258b93799503abb
Bhamini-Kashyap <84788109+Bhamini-Kashyap@users.noreply.github.com>
2022-09-19 21:56:40 +0530

    Create Day3_TranformArray.js

---
+++ Day3_TranformArray.js
@@ -0,0 +1,40 @@
+const { groupBy } = require("lodash");
+
+const recordset = [
+  {
+    roleId: 1,
+    roleTypeId: 1,
+    resourceId: 12,
+    resourceName: "Doe,John",
+    resourceEmail: "John.Doe@client.com",
+  },
+  {
+    roleId: 1,
+    roleTypeId: 1,
+    resourceId: 1,
+    resourceName: "Doe,Jane",
+    resourceEmail: "Jane.Doe@client.com",
+  },
+  {
+    roleId: 2,
+    roleTypeId: 1,
+    resourceId: 2,
+    resourceName: "Bob,Robert",
+    resourceEmail: "Robert.Bob@client.com",
+  },
+  {
+    roleId: 3,
+    roleTypeId: 1,
+    resourceId: 4,
+    resourceName: "Little,Stuart",
+    resourceEmail: "Stuart.Little@client.com",
+  },
+];
+
+//Tranform function for converting the array of objects into a nested array of objects, grouping them by roleId
+let result = Object.entries(groupBy(recordset, "roleId"));
+result = result.map((resourceItem) => ({
+  roleId: Number(resourceItem[0]),
+  resources: resourceItem[1],
+}));
+console.info("Transformed Object", result);
