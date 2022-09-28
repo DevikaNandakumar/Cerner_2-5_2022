c21ef46f3489570e8c469cd0819babcc63f75411
DevikaNandakumar <48020501+DevikaNandakumar@users.noreply.github.com>
2022-09-28 20:18:11 +0530

    Date and time

---
+++ Day13_CurrentDateTime.kt
@@ -0,0 +1,8 @@
+// cerner_2tothe5th_2022
+
+import java.time.LocalDateTime
+
+fun main(args: Array<String>) {
+    val current = LocalDateTime.now()
+    println("Current Date and Time is: $current")
+}
