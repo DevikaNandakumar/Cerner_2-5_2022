23903e550ac16d06b3e4c1e5942255da0fb707c5
DevikaNandakumar <48020501+DevikaNandakumar@users.noreply.github.com>
2022-10-06 10:26:11 +0530

    Array functions

---
+++ Day21_ArrayFunctions.pl
@@ -0,0 +1,12 @@
+# cerner_2tothe5th_2022
+
+@days = ("Mon","Tue","Wed");
+print "1st : @days\n";
+push(@days, "Thu");                                             
+print "2nd when push : @days\n";
+unshift(@days, "Fri");                                          
+print "3rd when unshift : @days\n";
+pop(@days);
+print "4th when pop : @days\n";                                 
+shift(@days);                                                   
+print "5th when shift : @days\n";
